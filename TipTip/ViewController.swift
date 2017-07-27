5f1eae86e6688634169746ab8c04445753dbe928
Kim Mok <cafe.mui@gmail.com>
2017-07-27 02:32:45 -0400

    implemented calculatingTip() the logic behind calculating the total amount

--- TipTip/ViewController.swift
+++ TipTip/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
 
     @IBAction func onTap(_ sender: Any) {
         //dismiss the keyboard
         view.endEditing(true)
     }
     
+    @IBAction func calculatingTip(_ sender: Any) {
+        //when textfield has change edits calculate the tip
+        let tipPercentage = [0.15, 0.2, 0.25]
+        
+        let bill = Double(billTextField.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
