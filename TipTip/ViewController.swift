b006ad8a613f883ded3af0aa3ed48c0400d7ec54
Kim Mok <cafe.mui@gmail.com>
2017-07-27 02:15:18 -0400

    change textfield keyboard type and include tap gesture to dismiss keyboard

--- TipTip/ViewController.swift
+++ TipTip/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onTap(_ sender: Any) {
+        //dismiss the keyboard
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
